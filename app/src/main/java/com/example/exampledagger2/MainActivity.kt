209de576e41115a1e00a94fbf460e08c0b59dc50
Bams <bambang.sianturi@alodokter.com>
2020-11-26 22:15:58 +0700

    remove todo

--- app/src/main/java/com/example/exampledagger2/MainActivity.kt
+++ app/src/main/java/com/example/exampledagger2/MainActivity.kt
@@ -13,8 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //TODO create code here bams
+        
         click_me.setOnClickListener {
             val getFight = melee.setEquip() + " and " + range.setEquip()
             report.text = getFight
